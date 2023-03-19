02be3639dda573da3acf541520d4e9dfb246dd77
Austin Kelsay <austinkelsay@yahoo.com>
2023-03-19 16:37:49 -0500

    Some prfile fixes

--- src/components/chat/ChatList.js
+++ src/components/chat/ChatList.js
@@ -1,87 +1,12 @@
-// import React, { useState, useEffect } from "react";
-// import { relayInit } from "nostr-tools";
-// import styles from "./chat.module.css";
-
-// const ChatList = () => {
-//   const [events, setEvents] = useState([]);
-//   const relay = relayInit("wss://relay.snort.social/");
-//   const sub = relay.sub([
-//     {
-//       kinds: [6847839],
-//     },
-//   ]);
-
-//   const checkAndReconnect = () => {
-//     if (relay.readyState === WebSocket.OPEN) {
-//       return;
-//     } else {
-//       relay.connect();
-
-//       sub.on("event", (event) => {
-//         console.log("event", event);
-//         setEvents((prevState) => [...prevState, event]);
-//       });
-
-//       relay.on("error", (err) => {
-//         console.error(`Relay err: ${err}`);
-//       });
-
-//       return () => {
-//         sub.unsub();
-//         relay.close();
-//       };
-//     }
-//   };
-
-//   useEffect(() => {
-//     checkAndReconnect();
-//   }, []);
-
-//   const formatTimestamp = (timestamp) => {
-//     const date = new Date(timestamp * 1000);
-//     return date.toLocaleDateString() + " " + date.toLocaleTimeString();
-//   };
-
-//   return (
-//     <div className={styles.chatList} style={{ overflowY: "scroll", height: "800px" }}>
-//       <ul className={styles.currentEvents}>
-//         {events.map((event) => (
-//           <li className={styles.event} key={event.id}>
-//             <div className={styles.eventContent}>
-//               <p>{event.content}</p>
-//               <span>{formatTimestamp(event.created_at)}</span>
-//               <div className={styles.eventHeader}>
-//                 <span>pubkey: {event.pubkey}</span>
-//               </div>
-//               <ul className={styles.eventTags}>
-//                 {event.tags.map((tag) => (
-//                   <li style={{ listStyle: "none" }} key={tag[1]}>
-//                     [{tag[0]}: {tag[1]}]
-//                   </li>
-//                 ))}
-//               </ul>
-//             </div>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default ChatList;
 import React, { useState, useEffect } from "react";
 import { relayInit } from "nostr-tools";
 import styles from "./chat.module.css";
 
 const ChatList = () => {
   const [events, setEvents] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // add isLoading state
+  const [isLoading, setIsLoading] = useState(true);
   const relay = relayInit("wss://relay.snort.social/");
-  const sub = relay.sub([
-    {
-      kinds: [6847839],
-    },
-  ]);
+  const sub = relay.sub([{ kinds: [6847839] }]);
 
   const checkAndReconnect = () => {
     if (relay.readyState === WebSocket.OPEN) {
@@ -115,14 +40,23 @@ const ChatList = () => {
     }
   }, [events]);
 
+  useEffect(() => {
+    setEvents((prevState) =>
+      [...prevState].sort((a, b) => b.created_at - a.created_at)
+    );
+  }, []); // only run once when the component mounts
+
   const formatTimestamp = (timestamp) => {
     const date = new Date(timestamp * 1000);
     return date.toLocaleDateString() + " " + date.toLocaleTimeString();
   };
 
   return (
-    <div className={styles.chatList} style={{ overflowY: "scroll", height: "800px" }}>
-      {isLoading ? ( // render loading text if isLoading is true
+    <div
+      className={styles.chatList}
+      style={{ overflowY: "scroll", height: "800px" }}
+    >
+      {isLoading ? (
         <div className={styles.error}>
           <div className={styles.loading}>
             <p>Loading...</p>
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -7,7 +7,7 @@ import QR from "../components/QR/QR";
 import Recent from "../components/Recent/Recent";
 import { useSession } from "next-auth/react";
 import styles from "../styles/profile.module.css";
-import Login from "../components/Onboarding/GithubLogin"
+import Login from "../components/Onboarding/GithubLogin";
 
 const Profile = () => {
   const [bio, setBio] = useState("");
@@ -15,13 +15,11 @@ const Profile = () => {
   const [avatarUrl, setAvatarUrl] = useState("");
 
   const { data: session, status } = useSession();
-  const user = session?.token?.login
+  const user = session?.token?.login;
 
   useEffect(() => {
     async function fetchGitHubData() {
-      const response = await fetch(
-        `https://api.github.com/users/${user}`
-      );
+      const response = await fetch(`https://api.github.com/users/${user}`);
       const data = await response.json();
       setUsername(data.login);
       setBio(data.bio);
@@ -32,8 +30,6 @@ const Profile = () => {
       fetchGitHubData();
     }
   }, [status, session]);
-
- 
 
   return (
     <>
@@ -56,9 +52,6 @@ const Profile = () => {
             <Box p="4">
               <ActiveRepos />
             </Box>
-            <Box p="4">
-              <LanguagesUsed />
-            </Box>
           </div>
           <div className={styles.aside}>
             <div className={styles.qr}>
@@ -67,12 +60,13 @@ const Profile = () => {
             <div className={styles.recent}>
               <Recent />
             </div>
+            <LanguagesUsed />
           </div>
         </div>
       ) : (
         <div className={styles.error}>
           <div className={styles.login}>
-          <h1>You are not logged in. Login to view your profile</h1>
+            <h1>You are not logged in. Login to view your profile</h1>
           </div>
         </div>
       )}
