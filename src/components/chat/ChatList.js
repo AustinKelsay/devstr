38b945467281788df0577643ddee8ebe4bcfd5ac
Medranomiler1 <medranomiler@gmail.com>
2023-03-19 15:45:33 -0500

    added loading message to chat page

--- src/components/chat/ChatList.js
+++ src/components/chat/ChatList.js
@@ -1,9 +1,81 @@
+// import React, { useState, useEffect } from "react";
+// import { relayInit } from "nostr-tools";
+// import styles from "./chat.module.css";
+
+// const ChatList = () => {
+//   const [events, setEvents] = useState([]);
+//   const relay = relayInit("wss://relay.snort.social/");
+//   const sub = relay.sub([
+//     {
+//       kinds: [6847839],
+//     },
+//   ]);
+
+//   const checkAndReconnect = () => {
+//     if (relay.readyState === WebSocket.OPEN) {
+//       return;
+//     } else {
+//       relay.connect();
+
+//       sub.on("event", (event) => {
+//         console.log("event", event);
+//         setEvents((prevState) => [...prevState, event]);
+//       });
+
+//       relay.on("error", (err) => {
+//         console.error(`Relay err: ${err}`);
+//       });
+
+//       return () => {
+//         sub.unsub();
+//         relay.close();
+//       };
+//     }
+//   };
+
+//   useEffect(() => {
+//     checkAndReconnect();
+//   }, []);
+
+//   const formatTimestamp = (timestamp) => {
+//     const date = new Date(timestamp * 1000);
+//     return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+//   };
+
+//   return (
+//     <div className={styles.chatList} style={{ overflowY: "scroll", height: "800px" }}>
+//       <ul className={styles.currentEvents}>
+//         {events.map((event) => (
+//           <li className={styles.event} key={event.id}>
+//             <div className={styles.eventContent}>
+//               <p>{event.content}</p>
+//               <span>{formatTimestamp(event.created_at)}</span>
+//               <div className={styles.eventHeader}>
+//                 <span>pubkey: {event.pubkey}</span>
+//               </div>
+//               <ul className={styles.eventTags}>
+//                 {event.tags.map((tag) => (
+//                   <li style={{ listStyle: "none" }} key={tag[1]}>
+//                     [{tag[0]}: {tag[1]}]
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ChatList;
 import React, { useState, useEffect } from "react";
 import { relayInit } from "nostr-tools";
 import styles from "./chat.module.css";
 
 const ChatList = () => {
   const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // add isLoading state
   const relay = relayInit("wss://relay.snort.social/");
   const sub = relay.sub([
     {
@@ -37,6 +109,12 @@ const ChatList = () => {
     checkAndReconnect();
   }, []);
 
+  useEffect(() => {
+    if (events.length > 0) {
+      setIsLoading(false);
+    }
+  }, [events]);
+
   const formatTimestamp = (timestamp) => {
     const date = new Date(timestamp * 1000);
     return date.toLocaleDateString() + " " + date.toLocaleTimeString();
@@ -44,26 +122,34 @@ const ChatList = () => {
 
   return (
     <div className={styles.chatList} style={{ overflowY: "scroll", height: "800px" }}>
-      <ul className={styles.currentEvents}>
-        {events.map((event) => (
-          <li className={styles.event} key={event.id}>
-            <div className={styles.eventContent}>
-              <p>{event.content}</p>
-              <span>{formatTimestamp(event.created_at)}</span>
-              <div className={styles.eventHeader}>
-                <span>pubkey: {event.pubkey}</span>
+      {isLoading ? ( // render loading text if isLoading is true
+        <div className={styles.error}>
+          <div className={styles.loading}>
+            <p>Loading...</p>
+          </div>
+        </div>
+      ) : (
+        <ul className={styles.currentEvents}>
+          {events.map((event) => (
+            <li className={styles.event} key={event.id}>
+              <div className={styles.eventContent}>
+                <p>{event.content}</p>
+                <span>{formatTimestamp(event.created_at)}</span>
+                <div className={styles.eventHeader}>
+                  <span>pubkey: {event.pubkey}</span>
+                </div>
+                <ul className={styles.eventTags}>
+                  {event.tags.map((tag) => (
+                    <li style={{ listStyle: "none" }} key={tag[1]}>
+                      [{tag[0]}: {tag[1]}]
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <ul className={styles.eventTags}>
-                {event.tags.map((tag) => (
-                  <li style={{ listStyle: "none" }} key={tag[1]}>
-                    [{tag[0]}: {tag[1]}]
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
